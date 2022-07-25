3ddf129b4f1992e0adae08acad64e6124f6506ed
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2022-07-25 21:49:42 +0900

    add dice game ui

--- lib/dice.dart
+++ lib/dice.dart
@@ -6,10 +6,54 @@ class Dice extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Dice Game!',
-      home: LogIn(),
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text('dice game!')
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(32.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Image(
+                  //   image: AssetImage('image/dice1.png'),
+                  //   width: 300.0,
+                  // )
+                  Expanded(
+                      child: Image.asset('image/dice1.png')
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  Expanded(
+                      child: Image.asset('image/dice2.png')
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 60.0,
+            ),
+            ButtonTheme(
+              minWidth: 100.0,
+              height: 60.0,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: Icon(Icons.play_arrow,
+                    color: Colors.white,
+                    size: 50.0,
+                  )
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
